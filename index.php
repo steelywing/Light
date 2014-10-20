548e49f5f43d4a7444d4b40af877da3045b5f6a8
Wing <steely.wing@gmail.com>
2014-10-20 15:14:01 +0800

    update

--- index.php
+++ index.php
@@ -5,6 +5,10 @@ include_once "vendor/autoload.php";
 use Light\Router;
 
 $router = Router::getInstance();
+$router->get(function () {
+	echo 'Home';
+});
+
 $router->setNotFound(function () use ($router) {
     print_r($router->env);
     print_r($_SERVER);
